b6dd3c2ca169aef083774b8f6053a0acc93bb7e6
troy2third <lsltry404@gmail.com>
2023-03-16 18:32:37 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/* use printf, no puts */
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: value of 0 indicates program runs successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
