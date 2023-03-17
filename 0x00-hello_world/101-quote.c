9eebf8b77a308a43836c48fcd5187ea93be8bfeb
troy2third <lsltry404@gmail.com>
2023-03-16 20:15:37 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/* UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity */
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: value 1 program was not successful
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
