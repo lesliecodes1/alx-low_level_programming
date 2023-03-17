6db837b2ea472d3eaf5f15fed3720186663b5757
troy2third <lsltry404@gmail.com>
2023-03-16 18:15:54 -0700

    Betty too,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 /* this my first C program */
 #include <stdio.h>
 /**
-*main - program entry point, void because no arguments passed
-*/
+ * main - program entry point, void because no arguments passed
+ * Return: - set to 0 program runs successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-/**
-*return value - set to 0 program runs successfully
-*/
 	return (0);
 }
