6412740430f28b149ad5fe86f27ba6f568316990
troy2third <lsltry404@gmail.com>
2023-03-21 16:44:22 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if alphabet,
+ * 0 otherwise
+ * Return: 1 for alphabet, 0 the rest
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
