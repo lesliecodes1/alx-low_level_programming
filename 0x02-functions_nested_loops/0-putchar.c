a12d49bda27ad5297138e2f9012d220c6bfd9db2
troy2third <lsltry404@gmail.com>
2023-03-21 10:21:45 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Description: print _putchar
+ * Return: value 0 always successful
+ */
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+<h2>0x02-functions_nested_loops</h2>   
+<h3>Program 0</h3>I prints _putchar, followed by a new line   
+
