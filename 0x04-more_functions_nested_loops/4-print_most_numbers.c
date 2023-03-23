ee7965362bbe75c7e751da50496c57205d3e7dc5
troy2third <lsltry404@gmail.com>
2023-03-23 16:35:50 -0700

    believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - exclude 2 and 4 from list
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (!(n == 50 || n == 52))
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
