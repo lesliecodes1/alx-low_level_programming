e10d4ad2524dacccde239481994b907cf0824802
troy2third <lsltry404@gmail.com>
2023-03-23 10:58:14 -0700

    Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
