67cb2ca2b606d1516e9b00fc765ac33ead1ca65e
troy2third <lsltry404@gmail.com>
2023-03-31 01:59:42 -0700

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,14 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	if (s1[i] > s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
 	}
-	else if (s1[i] < s2[i])
-	{
-		return (-15);
-	}
-	else
-		return (0);
+	return (0);
 }
