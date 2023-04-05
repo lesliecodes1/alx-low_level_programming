586b04fc1865947e0c9671a3cfd6bd16303aac6b
troy2third <lsltry404@gmail.com>
2023-04-05 16:06:17 -0700

    Idea: Brain under Siege

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns base x
+ * raised to power y
+ * @x: base number
+ * @y: power number
+ * Return: result of x power y,
+ * -1 if power < 0, 1 if power = 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int res;
+
+	res = 0;
+	if (y < 0)
+	{
+		return(-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		res = x * _pow_recursion(x, y - 1);
+	}
+	return (res);
+}
