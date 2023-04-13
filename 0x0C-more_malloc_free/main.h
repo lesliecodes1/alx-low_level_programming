d11f8416cbb5727106a9196eeb36fe37a02c1165
troy2third <lsltry404@gmail.com>
2023-04-13 03:55:27 -0700

    Mandatory Files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
