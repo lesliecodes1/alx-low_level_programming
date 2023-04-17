d1052555128b19fddc22b542ac1da569e57e825e
troy2third <lsltry404@gmail.com>
2023-04-17 13:18:43 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of source file compiled from
+ * Return: success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
