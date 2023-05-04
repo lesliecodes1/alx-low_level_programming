ad8e67610196387dafeaa662a3f4206288438a54
troy2third <lsltry404@gmail.com>
2023-05-04 13:27:21 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary rep of a number
+ * @n: number represented
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	printf("%ld", n & 1);
+}
