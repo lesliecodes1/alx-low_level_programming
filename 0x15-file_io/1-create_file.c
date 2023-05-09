9912876c78f85fc2698c6792a93a39915cdb2510
troy2third <lsltry404@gmail.com>
2023-05-08 17:04:06 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of created file
+ * @text_content: content to write to file
+ * Return: 1 if successful,
+ * -1 if unsuccessful
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int filedes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	ssize_t wr = write(filedes, text_content, strlen(text_content));
+
+	if (filename == NULL)
+		return (-1);
+	if (filedes == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (wr == -1)
+		{
+			close(filedes);
+			return (-1);
+		}
+	}
+	close(filedes);
+	return (1);
+}
