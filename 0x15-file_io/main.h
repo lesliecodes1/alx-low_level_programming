ba8ea758befdc7e555f16adfe0e90e2feaf7fe08
troy2third <lsltry404@gmail.com>
2023-05-08 18:53:15 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
