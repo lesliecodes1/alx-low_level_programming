2a1472c864e83b7882852be802d106612b05df6b
troy2third <lsltry404@gmail.com>
2023-05-15 02:01:02 -0700

    new correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int filedes = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
-	fchmod(filename, 0600);
 	ssize_t wr = write(filedes, text_content, strlen(text_content));
 
+	fchmod(filedes, 0600);
 	if (filename == NULL)
 		return (-1);
 	if (filedes == -1)
